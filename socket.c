2e409f6acb903a8b978a6b4e4923b1fc45d051b6
YazRaso <seyedyazdanrasoulzadeh@gmail.com>
2025-04-22 22:56:51 -0400

    Implemented server side sockets

--- socket.c
+++ socket.c
@@ -1,20 +1,63 @@
 #include <sys/socket.h>
 #include <netinet/in.h>
 #include <arpa/inet.h>
-#include <arpa/inet.h>
 #include <assert.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <unistd.h>
 // see sockets.h for documentation
-int create_socket(int port_number, char* ip_address) {
+int create_server_socket(int port_number, const char* ip_address, int max_connection_requests) {
     assert(ip_address);
     assert(port_number >= 0);
     assert(port_number <= 65535);
+    // creates stream socket (TCP) protocol on IPv$
     int socketFD = socket(AF_INET, SOCK_STREAM, 0);
+    if (socketFD < 0) {
+      perror("Failed to create socket");
+      return -1;
+    }
     struct sockaddr_in server_address;
+    // address family, IPv4 in this case
     server_address.sin_family = AF_INET;
+    // convert port_number to unsigned two byte integer
     server_address.sin_port = htons(port_number);
-    inet_pton(AF_INET, ip_address, &server_address.sin_addr.s_addr);
-    int result = connect(socketFD, &server_address, sizeof(server_address));
-    return result;
+    // convert ip address to be presentable to network
+    int presentation_to_network = inet_pton(AF_INET, ip_address, &server_address.sin_addr.s_addr);
+    // Enables socket option, SO_REUSEADDR
+    int optval = 1;
+    if (setsockopt(socketFD, SOL_SOCKET, SO_REUSEADDR, &optval, sizeof(optval)) < 0) {
+        perror("setsockopt");
+        // it is considered best practice to first make a shutdown call, before closing the socket
+        int shutdown_result = shutdown(socketFD, SHUT_RDWR);
+        if (shutdown_result == 0) {
+          // close file descriptor
+          close(socketFD);
+        }
+        return -1;
+    }
+    int bind_result = bind(socketFD, &server_address, sizeof(server_address));
+    int listen_result = listen(socketFD, max_connection_requests);
+    // Error handling
+
+    if (presentation_to_network == -1) {
+      perror("Failed to present to network");
+      return -1;
+    }
+    if (bind_result < 0) {
+      perror("Failed to bind");
+      int shutdown_result = shutdown(socketFD, SHUT_RDWR);
+      if (shutdown_result == 0) {
+        close(socketFD);
+      }
+      return -1;
+    }
+    if (listen_result < 0) {
+      perror("Failed to listen");
+      int shutdown_result = shutdown(socketFD, SHUT_RDWR);
+      if (shutdown_result == 0) {
+        close(socketFD);
+      }
+      return -1;
+    }
+    return socketFD;
 }
--- socket.h
+++ socket.h
@@ -1,3 +1,4 @@
-// create_socket(port_number, ip_address) creates a socket and listens on port_number on ip_address
+// create_socket(port_number, ip_address) binds a socket and listens on port_number on ip_address
+// accepting up to and including max_connection_requests
 // requires: 0 <= port_number <= 65535, ip_address must not be null
-int create_socket(int port_number, char* ip_address);
+int create_server_socket(int port_number, const char* ip_address, int max_connection_requests);
