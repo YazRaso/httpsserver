5228f069db5de9774009b9987629a7a4a22b91b8
YazRaso <seyedyazdanrasoulzadeh@gmail.com>
2025-04-22 22:57:43 -0400

    Refactored purpose

--- socket.h
+++ socket.h
@@ -1,4 +1,4 @@
-// create_socket(port_number, ip_address) binds a socket and listens on port_number on ip_address
+// create_server_socket(port_number, ip_address) binds a socket and listens on port_number on ip_address
 // accepting up to and including max_connection_requests
 // requires: 0 <= port_number <= 65535, ip_address must not be null
 int create_server_socket(int port_number, const char* ip_address, int max_connection_requests);
